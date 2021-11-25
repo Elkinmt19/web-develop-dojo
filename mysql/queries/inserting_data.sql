50e6718cd9db5239768200dbfd58c76e2fe1e50d
Elkinmt19 <elkin02011@hotmail.com>
2021-11-25 15:55:25 -0500

    Added some extra features to the existing queries and also added the <simple_queries.sql> file

--- mysql/queries/inserting_data.sql
+++ mysql/queries/inserting_data.sql
@@ -9,3 +9,12 @@ VALUES (1, 'Elkin Guerra', 'Artificial Intelligent'),
 
 -- To view the content of the table
 SELECT * FROM student;
+
+-- To update a specific row of a table 
+UPDATE student 
+SET major = 'AI'
+WHERE major = 'Artificial Intelligent';
+
+-- To delete a specific row of a table
+DELETE FROM student 
+WHERE student_id = 2;
