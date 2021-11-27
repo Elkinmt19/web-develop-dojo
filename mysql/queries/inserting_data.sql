177a68d603f7b6f8f0b48e99017a2c9ee3195830
Elkinmt19 <elkin02011@hotmail.com>
2021-11-26 20:52:08 -0500

    Make some changes to the queries of the mysql directory

--- mysql/queries/inserting_data.sql
+++ mysql/queries/inserting_data.sql
@@ -8,13 +8,13 @@ VALUES (1, 'Elkin Guerra', 'Artificial Intelligent'),
     (2, 'Leticia Guerra', 'Bacteriology');
 
 -- To view the content of the table
-SELECT * FROM student;
+SELECT * FROM `student`;
 
 -- To update a specific row of a table 
-UPDATE student 
-SET major = 'AI'
-WHERE major = 'Artificial Intelligent';
+UPDATE `student` 
+SET `major` = 'AI'
+WHERE `major` = 'Artificial Intelligent';
 
 -- To delete a specific row of a table
-DELETE FROM student 
-WHERE student_id = 2;
+DELETE FROM `student` 
+WHERE `student_id` = 2;
