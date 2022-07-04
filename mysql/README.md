eaa4c44bdf2ee1822b791cb0efedd5f6b6c995aa
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-07-03 22:27:51 -0500

    Deleted this project because it was upload it into order repo

--- mysql/README.md
+++
@@ -1,3 +0,0 @@
-# How to setup the MySQL environment to work 
-
-In order to setup an MySQL environment to work and do some SQL commands and queries, you must follow these steps:
--- mysql/db/refresh_root_config.sql
+++
@@ -1,4 +0,0 @@
--- SIMPLE ROOT-USER CONFIGURATION --
-ALTER USER 'root' @'localhost' IDENTIFIED BY 'root';
-ALTER USER 'root' @'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-FLUSH PRIVILEGES;
